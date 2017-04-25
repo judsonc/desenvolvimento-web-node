d96c21d75cafd0cdb7a27694895b7d45d5b1f70c
judsonc <judson.scosta@hotmail.com>
2017-04-25 20:32:22 -0300

    Novo nomes dos eventos

--- app.js
+++ app.js
@@ -13,16 +13,18 @@ function conectado(socket) {
   console.log('Requisição - ', new Date(), socket.id)
 
   // Retorna todos os tweets
-  socket.on('/get/tweets/', (data) => {
-    socket.emit('/get/tweets/', listaTweets)
-    console.log('tweets enviados', listaTweets)
-  })
+  socket.on('pedirTweets', pedirTweets)
+  function pedirTweets() {
+    socket.emit('todosTweets', listaTweets)
+    console.log('tweets enviados - ', listaTweets)
+  }
 
   // Insere um tweet
-  socket.on('/post/tweet/', (data) => {
+  socket.on('addTweet', addTweet)
+  function addTweet(data) {
     let novoTweet = new Tweet(data)
     listaTweets.push(novoTweet)
     console.log('um tweet inserido ', novoTweet)
-    io.emit('/get/tweets/', listaTweets)
-  })
+    io.emit('todosTweets - ', listaTweets)
+  }
 }
