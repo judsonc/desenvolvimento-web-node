3fae21e173d1f5cf0a353a5fc20e31bb79d0b84d
judsonc <judson.scosta@hotmail.com>
2017-05-06 00:02:51 -0300

    Ordem decrescente de tweets

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const Tweet = require('./model/tweet') // importação do modelo Tweet
 
 io.on('connection', conectado)
 function conectado(socket) {
-  console.log('\nRequisição - ', new Date(), socket.id)
+  console.info('\nRequisição - ', new Date(), socket.id)
 
   // Retorna todos os tweets
   socket.on('pedirTweets', pedirTweets)
@@ -17,7 +17,7 @@ function conectado(socket) {
     fs.readFile('./tweets.json', (err, tweets) => {
       let listaTweets = JSON.parse(tweets)
       socket.emit('todosTweets', listaTweets)
-      console.log('\nTweets enviados - ', listaTweets)
+      console.info('\nTweets enviados - ', listaTweets)
     })
   }
 
@@ -27,10 +27,10 @@ function conectado(socket) {
     let novoTweet = new Tweet(data)
     fs.readFile('./tweets.json', (err, tweets) => {
       let listaTweets = JSON.parse(tweets)
-      listaTweets.push(novoTweet)
+      listaTweets.unshift(novoTweet)
       fs.writeFile('./tweets.json', JSON.stringify(listaTweets, null, 2), (err) => {
         if (err) console.log(err)
-        console.log('\nUm tweet inserido - ', novoTweet)
+        console.info('\nUm tweet inserido - ', novoTweet)
         io.emit('todosTweets', listaTweets)
       })
     })
@@ -45,7 +45,7 @@ function conectado(socket) {
       listaTweets.map(curtirPorId)
       fs.writeFile('./tweets.json', JSON.stringify(listaTweets, null, 2), (err) => {
         if (err) console.log(err)
-        console.log('\nUm tweet curtido - ', id)
+        console.info('\nUm tweet curtido - ', id)
         io.emit('todosTweets', listaTweets)
       })
     })
